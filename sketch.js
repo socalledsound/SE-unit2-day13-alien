9b3e99c4e8a340fde11d592698f9eb0cccf5b131
Chris Kubick <socalledsound@gmail.com>
2020-10-05 21:32:08 -0700

    finished alien

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 
+const numImgs = 21;
+const imgArray = Array.from({ length: numImgs})
+let counter = 0;
+let x = 100, y = 100;
+
+
 function preload(){
+
+  imgArray.forEach((item,i) => {
+    imgArray[i] = loadImage(`img/alien${i}.png`);
+  })
 
 }
 
@@ -10,6 +20,23 @@ function setup() {
 
 function draw() {
   background(220);
-  
+  image(imgArray[counter%numImgs], x, y, 250, 250); 
+  counter++
+}
+
+function keyPressed(){
+
+  if (keyCode === LEFT_ARROW) {
+    x-=10;
+  } else if (keyCode === RIGHT_ARROW) {
+    x+=10;
+  } else if(keyCode === UP_ARROW){
+    y-=10;
+  } else if(keyCode === DOWN_ARROW){
+    y+=10;
+    
+  }
+
+
 }
 
